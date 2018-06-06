ee59c4333ea815df6b341004887ddb71326465b2
love1 <love1dbsb77@hotmail.com>
2018-06-05 22:07:20 -0700

    Added Create blog page but the action is not working

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,5 +2,18 @@ class BlogsController < ApplicationController
     def index
         @blogs = Blog.all
     end
+
+    def new
+        @blog = Blog.new
+    end
+
+    def create
+        @blog = Blog.new(params[:title, :content])
+        if @blog.save
+            redirect_to '/blogs'
+        else
+            render 'new'
+        end
+    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL avaeilable within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'blogs' => 'blogs#index'
+  get 'blogs/new' => 'blogs#new'
+  post 'blogs' => 'blogs#create'
 end
